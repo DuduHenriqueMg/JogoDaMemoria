05fd3c12f3945154174aa5b98d6cad17efaa7235
DuduHenriqueMg <dudugama03@gmail.com>
2022-09-28 03:23:11 -0300

    Commit final

--- scripts/game.js
+++ scripts/game.js
@@ -1,18 +1,72 @@
 
+
 let game = {
 
-    teams : ['arsenal',
-        'barcelona',
-        'real',
-        'atletico',
-        'milan',
-        'internazionale',
-        'chelsea',
-        'liverpool',
-        'city',
-        'united',],
+    lockMode : false,
+    firstCard : null,
+    secondCard : null,
 
-    cards : null,    
+    teams : ['arsenal',
+    'barcelona',
+    'real',
+    'atletico',
+    'milan',
+    'internazionale',
+    'chelsea',
+    'liverpool',
+    'city',
+    'united',],
+
+    cards : null, 
+
+    setCard : function(id) {
+
+        let card = this.cards.filter(card=>card.id === id) [0];
+        console.log(card);
+        if (card.flipped || this.lockMode ) {
+            return false;
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        }else{
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    checkMatch: function(){
+        if (!this.firstCard || !this.secondCard) {
+            return false;
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+        
+
+    },
+
+    clearCards: function(){
+
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+
+    },
+
+    unflipCards(){
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+    checkGameOver(){
+        return this.cards.filter(card =>! card.flipped).length == 0;
+    },
+  
 
     createCardFromTeams : function() {
 
